fad87af605fb879a7ad1c4bff6a6e42addc61480
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2020-02-22 17:15:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n)  
+    }    
+    let result = n.toString()
+    .split('')
+    .reverse()
+    .join('')
+    
+    return  +result
 }
